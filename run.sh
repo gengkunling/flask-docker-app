523a937bb27a9f0ece62fbea89509c50496023ab
Kunling <gengkunling@gmail.com>
2019-04-30 16:39:29 -0700

    first commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+sudo apt install docker.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+docker build -t gengkunling/python-flask-docker .
+
+docker run --name my-container -d -p 80:80 gengkunling/python-flask-docker
