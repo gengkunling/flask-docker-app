3924ca06066fc1115771968ffbb849c88449c769
Kunling <gengkunling@gmail.com>
2019-04-30 17:10:02 -0700

    change

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 sudo apt install docker.io
-sudo apt install git-all
+
 
 sudo systemctl start docker
 sudo systemctl enable docker
 
-git clone https://github.com/gengkunling/flask-docker-app.git
-cd flask-docker-app
 
 docker build -t gengkunling/python-flask-docker .
 
